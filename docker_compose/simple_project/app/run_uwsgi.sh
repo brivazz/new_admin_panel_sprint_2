aebf31731399eb912540e4b92fb0b7c2ae6644cd
Sergey Toporets <yateperna@yandex.ru>
2023-03-24 14:54:05 +0300

    Убрал возникновение ошибки в run_uwsgi.sh при повторном создании superuser

--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
 sleep 10
+set -e
 
 python manage.py makemigrations
 python manage.py migrate
@@ -10,6 +10,7 @@ python manage.py collectstatic --noinput
 python manage.py createsuperuser \
     --noinput \
     --username $DJANGO_SUPERUSER_USERNAME \
-    --email $DJANGO_SUPERUSER_EMAIL
+    --email $DJANGO_SUPERUSER_EMAIL \
+    || true
 
 uwsgi --strict --ini uwsgi/uwsgi.ini
